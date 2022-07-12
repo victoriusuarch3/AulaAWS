921bd9de07a814bf623c0376933538cf943de46f
Víctor Vitoriano <victorfrance20@yahoo.com>
2022-07-12 20:31:12 -0300

    Projeto AWS

---
+++ Aula.AWS.Web/Program.cs
@@ -0,0 +1,31 @@
+using Amazon.Runtime;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var awsOptions = builder.Configuration.GetAWSOptions();
+awsOptions.Credentials = new EnvironmentVariablesAWSCredentials();
+builder.Services.AddDefaultAWSOptions(awsOptions);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
